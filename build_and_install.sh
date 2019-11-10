80a53ffa23db3e488335abfec42c4887a6378448
Josh Roy <josh_roy@brown.edu>
2019-11-09 21:32:58 -0500

    building

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bazel build -c opt python/pip_package:build_pip_package && ./bazel-bin/python/pip_package/build_pip_package /tmp/dmlab_pkg && pip install /tmp/dmlab_pkg/DeepMind_Lab-1.0-py2-none-any.whl --force-reinstall
+~/bin/bazel build -c opt python/pip_package:build_pip_package && ./bazel-bin/python/pip_package/build_pip_package /tmp/dmlab_pkg && pip install /tmp/dmlab_pkg/DeepMind_Lab-1.0-py2-none-any.whl --force-reinstall
